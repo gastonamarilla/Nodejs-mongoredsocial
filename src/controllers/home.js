da4898627f1db2d14fe3b4af8880d9202647bac8
Gaston <gastonamarilla2013@gmail.com>
2020-06-01 16:27:23 -0300

    Error con find

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,9 @@
+const {Image} = require('../models');
 const ctrl = {};
 
-ctrl.index = (req,res) =>{
-    res.send('Index page');
-}
+ctrl.index =  async (req,res) =>{
+    const images = await Image.find().sort({ timestamp: -1 });
+    res.render('index', {images});
+};
 
 module.exports = ctrl;
--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,10 +1,43 @@
+const path = require('path');
 const ctrl =  {};
+const {randomNumber} = require('../helpers/libs');
+const fs = require ('fs-extra');
+const { image } = require('../models/index');
 
 ctrl.index = (req,res) =>{
 
 };
-ctrl.create = (req,res) =>{
-
+ctrl.create =  (req,res) =>{ 
+    const saveimage = async () =>{
+        const imageURL = randomNumber();
+        const images = await Image.find({filename: imageURL});
+        if (images.length > 0){
+            saveimage();
+        } else{
+            console.log(imageURL);
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();  
+            const targetPath = path.resolve(`src/public/upload/${imageURL}${ext}`)
+            if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+               await fs.rename(imageTempPath, targetPath);
+             const newImg =  new image ({
+                   title: req.body.title,
+                   filename: imageURL + ext,
+                   description: req.body.description
+               });
+               const imageSaved =  await newImg.save();
+              // res.redirect('/images');
+              res.send('works!');
+            }
+            else{
+              await  fs.unlink(imageTempPath);
+              res.status(500).json({error: 'Only images are allowed'});
+            }
+            res.send('works!');
+        }
+      
+    };
+    saveimage();
 };
 
 ctrl.like = (req,res) => {
